8cd12cc4a0965d58ee229e275cc20686658abab2
ShaunakDave09 <shaunakdave09@gmail.com>
2021-08-02 15:44:37 +0530

    Complete User group views and user rights view

--- src/views/pages/user-management/user-rights/RightsProfile.js
+++ src/views/pages/user-management/user-rights/RightsProfile.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // material-ui
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,6 +13,7 @@ import { gridSpacing } from "../../../../store/constant";
 
 // import ErrorTwoToneIcon from "@material-ui/icons/ErrorTwoTone";
 import useCompany from "../../../../hooks/useCompany";
+import useAuth from "../../../../hooks/useAuth";
 // import FormControlSelect from "../../../../ui-component/extended/Form/FormControlSelect";
 
 // style constant
@@ -33,24 +34,56 @@ const useStyles = makeStyles((theme) => ({
 
 const RightsProfile = () => {
   const classes = useStyles();
+  const { user } = useAuth();
 
   const { currentCompany, updateForm } = useCompany();
+
+  const [values, setValues] = useState({
+    "user_group_id":3,
+    "transaction_id":4,
+    "can_create":true,
+    "can_alter":true,
+    "can_delete":true,
+    "can_view":true,
+    "created_by":"harsh@gmail.com"
+})
+
+const handleChange = (event) => {
+    setValues({
+      ...values,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+const handleChecked = (event) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.checked,
+    });
+  };
 
   return (
     
     <Grid container spacing={gridSpacing}>
+      <Grid item xs={12}>
+        <pre>{JSON.stringify(values, null, 2)}</pre>
+      </Grid>
+
       <Grid item xs={12} sm={6}>
         <TextField
           fullWidth
-          label="Name"
+          label="User Group - ID"
+          type="number"
           value={currentCompany.name}
           InputLabelProps={{ shrink: true }}
           onChange={(e)=>{updateForm({name:e.target.value})}}
         />
       </Grid>
+
       <Grid item xs={12} sm={6}>
-        <TextField fullWidth label="email" type="email" value={currentCompany.email} InputLabelProps={{ shrink: true }} onChange={(e)=>{updateForm({email:e.target.value})}}/>
+        <TextField fullWidth label="Transaction ID" type="number" value={currentCompany.email} InputLabelProps={{ shrink: true }} onChange={(e)=>{updateForm({email:e.target.value})}}/>
       </Grid>
+
       <Grid item xs={12} sm={6}>
         <Grid container direction="row" spacing={3}>
           <Grid item xs={12}>
@@ -58,28 +91,32 @@ const RightsProfile = () => {
               User Groups Related Fields
             </Typography>
           </Grid>
+
           <Grid item xs={12}>
             <Grid container direction="column" spacing={0}>
               <Grid item>
                 <FormControlLabel
                   control={
-                    <Switch checked={true} onChange={()=>{return null}} name="checkedA" color="primary" />
-                  }
-                  label="Can View"
-                />
-              </Grid>
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <Switch checked={true} onChange={()=>{return null}} name="checkedB" color="primary" />
+                    <Switch checked={true} 
+                      onChange={handleChecked}
+                      name="can_create"
+                      id="can_create" 
+                      checked={values.can_create}
+                      color="primary" />
                   }
                   label="Can Create"
                 />
               </Grid>
               <Grid item>
+
                 <FormControlLabel
                   control={
-                    <Switch checked={true} onChange={()=>{return null}} name="checkedB" color="primary" />
+                    <Switch 
+                      onChange={handleChecked}
+                      name="can_alter"
+                      id="can_alter" 
+                      checked={values.can_alter}
+                      color="primary" />
                   }
                   label="Can Edit"
                 />
@@ -87,7 +124,24 @@ const RightsProfile = () => {
               <Grid item>
                 <FormControlLabel
                   control={
-                      <Switch checked={true} onChange={()=>{return null}} name="checkedC" color="primary" />
+                    <Switch 
+                      onChange={handleChecked}
+                      name="can_view"
+                      id="can_view" 
+                      checked={values.can_view}
+                      color="primary" />
+                  }
+                  label="Can View"
+                />
+              </Grid>
+              <Grid item>
+                <FormControlLabel
+                  control={
+                      <Switch name="can_delete"
+                        onChange={handleChecked}
+                        id="can_delete" 
+                        checked={values.can_delete}
+                        color="primary" />
                   }
                   label="Can Delete"
                 />
